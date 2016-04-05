964a941a7f4b0377c553bf2fbe8e790a1515b8a8
goldtortoise <donat.delgado@gmail.com>
2016-04-05 19:00:43 +0900

    Initial Commit

---
+++ RENTR/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RENTR
+//
+//  Created by kgh on 4/5/16.
+//  Copyright (c) 2016 kgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
